dad682332ee9cbee77e4a35c2b07738cca78b525
Winters67 <emmanuel.vbg@gmail.com>
2022-05-16 09:30:46 +0200

    modification du fichier et ajout des corrections

--- VerbruggeEmmanuel_6_29042022/backend/middleware/password-validator.js
+++ VerbruggeEmmanuel_6_29042022/backend/middleware/password-validator.js
@@ -1,11 +1,11 @@
-const passwordValidator = require('password-validator');
+const passwordValidator = require("password-validator");
 
 const passwordSchema = new passwordValidator();
 
 // prettier-ignore
 passwordSchema
-    .is().min(4)                                    // Minimum length 8
-    .is().max(50)                                   // Maximum length 100
+    .is().min(4)                                    // Minimum length 4
+    .is().max(50)                                   // Maximum length 50
     .has().uppercase()                              // Must have uppercase letters
     .has().lowercase()                              // Must have lowercase letters
     .has().digits(2)                                // Must have at least 2 digits
