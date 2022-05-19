8f956956b86d8c56076124f9a5697892bc1d8d71
Winters67 <emmanuel.vbg@gmail.com>
2022-05-19 09:26:11 +0200

    variable .env

--- VerbruggeEmmanuel_6_29042022/backend/app.js
+++ VerbruggeEmmanuel_6_29042022/backend/app.js
@@ -12,10 +12,15 @@ const sauceRoutes = require("./routes/sauce");
 //---------analyse le corps de la requête transforme en  json--------------
 app.use(express.json());
 
-// ----------connection à la base de données mongoose--------
+// permettre l'utilisation de variables d'environnement
+require("dotenv").config();
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
+// connection à la base de données mongoose
 mongoose
   .connect(
-    "mongodb+srv://Winters:E290380l@cluster0.9vp7c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.9vp7c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => console.log("Connexion à MongoDB réussie !"))
